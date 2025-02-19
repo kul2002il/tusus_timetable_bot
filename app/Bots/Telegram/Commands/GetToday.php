2c357558a5b8f7ef1290361dee063559be364a48
Ilya V. Kulmanakov <ikulmanakov@tomsksoft.com>
2025-02-19 15:26:07 +0700

    Schedule timetable pull

--- app/Bots/Telegram/Commands/GetToday.php
+++ app/Bots/Telegram/Commands/GetToday.php
@@ -22,6 +22,11 @@ class GetToday extends AbstractCommand
         /** @var Day $day */
         $day = $subscription->group->days()->where('date', Carbon::now()->toDateString())->latest('created_at')->first();
 
+        if (!$day) {
+            $this->response('В настоящее время расписание ещё не загружено (обновление каждые 15 минут), либо пар нет.');
+            return;
+        }
+
         $this->response(
             view('bot.day', [
                 'lessons' => json_decode($day->body),
