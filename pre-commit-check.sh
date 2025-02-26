e496ffa616ca54ad107443b3bed9a2fb0ee08dd4
ilia <kul.2002.il@gmail.com>
2025-02-26 14:57:00 +0700

    Fix pre-commit-check.sh

--- pre-commit-check.sh
+++ pre-commit-check.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+
 ./vendor/bin/sail bash -c "PHP_CS_FIXER_IGNORE_ENV=1 php ./vendor/bin/php-cs-fixer fix"
 ./vendor/bin/sail bash -c "./vendor/bin/phpstan analyse app tests"
 ./vendor/bin/sail artisan test
+
